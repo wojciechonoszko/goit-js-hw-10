a42d9d2fcdbd01d5fe266791d712a14dfac75d00
Wojciech <wojciechonoszko@gmail.com>
2022-04-03 15:32:25 +0200

    updating

--- src/index.js
+++ src/index.js
@@ -4,22 +4,13 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 
-const fetchUsersBtn = document.querySelector(".btn");
-const userList = document.querySelector(".user-list");
 const countryList = document.querySelector(".country-list");
 let input = document.querySelector('input');
 
 
 console.log(input.value.trim())
 
-//input.addEventListener('input', doThing());
 
-// input.addEventListener("input", _.debounce(() => {
-//   fetchUsers()
-//     .then((users) => renderUserList(users))
-//     .catch((error) => console.log(error));
-// }, 300)
-// );
 
 input.addEventListener("input", _.debounce(() => {
   
@@ -29,23 +20,7 @@ input.addEventListener("input", _.debounce(() => {
 }, 300)
 );
 
-function doThing(){
-  console.log('Horray! Someone wrote "' + input + '"!"')
-};
 
-
-
-
-
-// fetchUsersBtn.addEventListener("click", () => {
-//   fetchUsers()
-//     .then((users) => renderUserList(users))
-//     .catch((error) => console.log(error));
-// });
-
-
-
-// const searchParams = input.value;
 
 function fetchCountries() {
 
@@ -57,30 +32,10 @@ function fetchCountries() {
         
       }
       return response.json();
-      //return fetchCountries.json();
     }
   )
-  // .then((data) => {
-  //   console.log(data);
-  // })
-  // .catch((error) => {});
 }
 
-// function renderUserList(users) {
-//   const markup = users
-  
-//     .map((user) => {
-//       return `<li>
-//           <p><b>Name</b>: ${user.name.official}</p>
-//           <p><b>Capital</b>: ${user.capital}</p>
-//           <p><b>Population</b>: ${user.population}</p>
-//           <p><b>Flags</b>: ${user.flags.svg}</p>
-//           <p><b>languages</b>: ${user.languages}</p>
-//         </li>`;
-//     })
-//     .join("");
-//   userList.innerHTML = markup;
-// }
 
 function renderCountryList(countries) {
   console.log(countries.length)
